d57edbbd68118b7fcb14a122bc11c00f5990b566
shailendra sahu <sahushailendra73@gmail.com>
2025-11-14 16:38:09 +0530

    im

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -12,18 +12,9 @@ const allowedOrigins = [
 ]
 app.use(
   cors({
-    origin: function (origin, callback) {
-      // allow requests with no origin (like Postman)
-      if (!origin) return callback(null, true);
-
-      if (allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
+    origin: allowedOrigins, // frontend origin (Vite dev server)
+    credentials: true, // allow cookies, JWT in cookies
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   })
 );
 app.use(express.json())  // for parsing application/json

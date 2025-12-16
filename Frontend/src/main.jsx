ccce30ae16498f2ed3e870e6f0f80e194a41fc76
shailendra sahu <sahushailendra73@gmail.com>
2025-12-17 00:25:58 +0530

    login

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,15 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { RouterProvider,  createBrowserRouter } from 'react-router-dom'
-import Feed from './Components/Feed.jsx'
-import Profile from './pages/Profile.jsx'
-import Login from './pages/Login.jsx'
-import Signup from './pages/Signup.jsx'
-import { Provider } from 'react-redux'
-import store from './store/store.jsx'
-import ProtectedRoute from './Components/ProtectedRoute.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Feed from "./Components/Feed.jsx";
+import Profile from "./pages/Profile.jsx";
+import Login from "./pages/Login.jsx";
+import Signup from "./pages/Signup.jsx";
+import { Provider } from "react-redux";
+import store from "./store/store.jsx";
+import ProtectedRoute from "./Components/ProtectedRoute.jsx";
 
 const router = createBrowserRouter([
   {
@@ -18,26 +18,34 @@ const router = createBrowserRouter([
     children: [
       {
         path: "/",
-        element: <ProtectedRoute><Feed /></ProtectedRoute>,
+        element: (
+          <ProtectedRoute>
+            <Feed />
+          </ProtectedRoute>
+        ),
       },
       {
         path: "/profile/:id",
-        element: <ProtectedRoute><Profile /></ProtectedRoute>,
+        element: (
+          <ProtectedRoute>
+            <Profile />
+          </ProtectedRoute>
+        ),
       },
-      {
-        path: "/login",
-        element: <Login />,
-      },
-      {
-        path: "/signup",
-        element: <Signup />,
-      }
     ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
   },
 ]);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <RouterProvider router={router} />
   </Provider>
-)
+);
